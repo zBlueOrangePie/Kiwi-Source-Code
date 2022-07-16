2b1877ed55f6351e1f770e2ec192c32d26f9add2
zBlueOrangePie <100342472+Joshua-2010@users.noreply.github.com>
2022-07-16 12:04:07 +0800

    Add files via upload

---
+++ commands/fun/howgay.js
@@ -0,0 +1,8 @@
+module.exports =({
+    name: "howgay",
+    code: `
+$title[Howgay Are You?]
+$description[You Are $randomText[1;2;3;4;5;6;7;8;9;10;11;12;32;34;54;56;43;67;89;99;100;45;32,28;39;40;52]% Gay!]
+$footer[lol]
+$color[$getServerVar[colorid]]
+})
---
+++ commands/fun/slap.js
@@ -0,0 +1,7 @@
+module.exports =({
+
+  name: 'slap',
+
+  code: `$argsCheck[1;**Please mention somebody to slap.**]?$color[$randomText[FF0000;00BFFF]]?$title[** SOMEONE GOT SLAP **]?$description[** 🖐 <@$authorID> SLAPPED <@$mentioned[1]> 😱 ** ]$image[https://media1.giphy.com/media/mEtSQlxqBtWWA/giphy.gif?cid=19f5b51a5d0007c26844576955890a26&rid=giphy.gif]`
+
+})
