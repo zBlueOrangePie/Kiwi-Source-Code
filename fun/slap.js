f53d25ac3b958baa61c712b46208c21b09906abf
zBlueOrangePie <100342472+Joshua-2010@users.noreply.github.com>
2022-07-16 12:01:28 +0800

    Delete slap.js

--- fun/slap.js
+++
@@ -1,7 +0,0 @@
-module.exports =({
-
-  name: 'slap',
-
-  code: `$argsCheck[1;**Please mention somebody to slap.**]?$color[$randomText[FF0000;00BFFF]]?$title[** SOMEONE GOT SLAP **]?$description[** 🖐 <@$authorID> SLAPPED <@$mentioned[1]> 😱 ** ]$image[https://media1.giphy.com/media/mEtSQlxqBtWWA/giphy.gif?cid=19f5b51a5d0007c26844576955890a26&rid=giphy.gif]`
-
-})
