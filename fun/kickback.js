0fcba94a0d942295babd7b11ad4db945f6ff7254
zBlueOrangePie <100342472+Joshua-2010@users.noreply.github.com>
2022-07-16 12:01:39 +0800

    Delete kickback.js

--- fun/kickback.js
+++
@@ -1,7 +0,0 @@
-module.exports =({
-
-  name: 'kickback',
-
-  code: `$title[$username kickback $username[$mentioned[1]]!]$description[Oh!]$color[000000]$image[https://c.tenor.com/Lyqfq7_vJnsAAAAM/kick-funny.gif]$argsCheck[1;You havent written who you want to kickback]`
-
-})
