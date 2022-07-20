1aa16d08901707539f77ac97bcc2e7a30f92a7e1
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2022-07-20 20:59:24 +0800

    Create withdraw.js

---
+++ commands/economy/withdraw.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: 'withdraw',
+  aliases: 'with',
+  code: `$setGlobalUserVar[Wallet;$sum[$getGlobalUserVar[Wallet;$authorID];$message];$authorID]
+$setGlobalUserVar[Bank;$sub[$getGlobalUserVar[Bank;$authorID];$message];$authorID]
+$title[Withdrew]
+$thumbnail[$userAvatar[$authorID]]
+$color[RANDOM]
+$description[
+$username, you withdrew $$numberSeparator[$message] from your bank!]
+$footer[💵 $$numberSeparator[$sum[$getGlobalUserVar[Wallet;$authorID];$message]] | 🏦 $$numberSeparator[$sub[$getGlobalUserVar[Bank;$authorID];$message]] | 📊 $$numberSeparator[$sum[$getGlobalUserVar[Wallet;$authorID];$getGlobalUserVar[Bank;$authorID]]]]
+$onlyIf[$isNumber[$message[1]]==true;That's not a number!]
+$onlyIf[$message<=$getGlobalUserVar[Bank;$authorID];Cannot withdraw more than what's in your bank!]
+$argsCheck[>1;How much are you withdrawing?]
+$onlyIf[$getGlobalUserVar[Bank;$authorID]>0;There's nothing to withdraw!]`
+}
