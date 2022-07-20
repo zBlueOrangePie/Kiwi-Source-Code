1e3ac100471ed87fc7d68eab040cd28fa2aec080
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2022-07-20 20:58:36 +0800

    Edited help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -4,9 +4,11 @@ module.exports =({
     code: `
 <@$authorID> Here Are All Of My Bot Commands
 $title[Bot Commands]
-$addField[Fun Commands; k!math, k!kickback, k!howgay, k!slap]
-$addField[Information Commands; k!info, k!ping, k!help, k!uptime]
+$addField[Fun Commands; k!calculator, k!kickback, k!howgay, k!slap]
+$addField[Information Commands; k!info, k!ping, k!help, k!uptime k!commandinfo]
 $addField[Utility Commands; k!say]
+$addField[Moderation Commands;k!ban, k!kick, k!unban, k!mute, k!unmute, k!tempban, k!setmute, k!purge, k!removerole, k!giverole, k!temprole]
+$addField[Economy Commands;k!balance, k!deposit, k!withdraw, k!work, k!beg]
 $footer[@Kiwi - Bot]
 $color[$getServerVar[colorid]]`
 })
