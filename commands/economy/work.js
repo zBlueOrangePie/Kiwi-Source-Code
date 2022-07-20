ee3863318bd637d327cf623c3f4c562daf72f99e
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2022-07-20 21:06:58 +0800

    Create work.js

---
+++ commands/economy/work.js
@@ -0,0 +1,22 @@
+module.exports =({
+    name: "work",
+    code: `$setGlobalUserVar[Wallet;$sum[$getGlobalUserVar[Wallet;$authorID];$random[45;100]];$authorID] 
+
+$setGlobalUserVar[XP;$sum[$getGlobalUserVar[XP;$authorID];$random[5;10]];$authorID] 
+
+$thumbnail[$userAvatar[$authorID]] 
+
+$color[RANDOM] 
+
+$title[Work] 
+
+$description[ 
+
+$username, $randomText[it looks like you'd do anything for money 😳.;you were employed as a construction worker today!;you're a discord workaholic!;nice music dude! Here, you earned it!;you were employed as a hair stylist today;hacking can make you a good deal of money if you know what you're doing! From your last hack job, you made;your bitcoin miner scraped up some cash for you!;were you not satisfied with your past employers? Well I counted the tips from your pole dancing gig and you didn't do too bad 😳] 
+
+] 
+
+$footer[💵 +$$random[45;100] | 🗡 +$random[5;10]xp] 
+
+$globalCooldown[60s;Try again in %time%]`
+})
