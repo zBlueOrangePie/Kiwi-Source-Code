4a2f20f15235b77168c5052b5216b4fd77eb596d
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2022-07-20 21:10:04 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+module.exports ={
+    colorid: "#14dcb4",
+    devid: "896709705248960512",
+    globallogsid: "996998283979407460",
+    Wallet: "0",
+    XP: "0",
+    Bank: "0",
+    prefix: "k!",
+    muted: ""
+}
