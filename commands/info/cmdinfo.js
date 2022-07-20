a72220332e0c0f574f8a47176c6426f0d6707766
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2022-07-20 20:55:39 +0800

    Create cmdinfo.js

---
+++ commands/info/cmdinfo.js
@@ -0,0 +1,12 @@
+module.exports =({
+    name: "commandinfo",
+    code: `
+$title[Updates Of My Commands]
+$addField[Economy Commands;🟡 Beta]
+$addField[Information Commands;🟢 Good]
+$addField[Moderation Commands;🟡 Beta]
+$addField[Fun Commands;🟢 Good]
+$addField[Utility Commands;🔴 Updating]
+$color[$getServerVar[colorid]]
+$footer[All Commands Info]`
+})
