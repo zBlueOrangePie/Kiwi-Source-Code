ffd32330c87a8089d5f50b23330f1fa894ddfa0f
zBlueOrangePie <100342472+zBlueOrangePie@users.noreply.github.com>
2022-07-21 10:36:39 +0800

    Update howgay.js

--- commands/fun/howgay.js
+++ commands/fun/howgay.js
@@ -4,5 +4,5 @@ module.exports =({
 $title[Howgay Are You?]
 $description[You Are $randomText[1;2;3;4;5;6;7;8;9;10;11;12;32;34;54;56;43;67;89;99;100;45;32,28;39;40;52]% Gay!]
 $footer[lol]
-$color[$getServerVar[colorid]]
+$color[$getServerVar[colorid]]`
 })
